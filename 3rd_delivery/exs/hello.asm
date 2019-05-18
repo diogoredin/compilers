0a959df2932b4609c585d87249830a9602faf217
Diogo Redin <diogo.redin@gmail.com>
2019-05-18 19:48:29 +0100

    'Hello World' compiles and works

--- 3rd_delivery/exs/hello.asm
+++ 3rd_delivery/exs/hello.asm
@@ -1,5 +1,5 @@
 ; EXTRN
-extern	$prints
+extern	$_prints
 ; TEXT
 segment	.text
 ; ALIGN
@@ -12,6 +12,48 @@ $_entry:
 	push	ebp
 	mov	ebp, esp
 	sub	esp, 4
+; RODATA
+segment	.rodata
+; ALIGN
+align	4
+; LABEL
+$_i1:
+; CHAR
+	db	0x48
+; CHAR
+	db	0x65
+; CHAR
+	db	0x6C
+; CHAR
+	db	0x6C
+; CHAR
+	db	0x6F
+; CHAR
+	db	0x20
+; CHAR
+	db	0x57
+; CHAR
+	db	0x6F
+; CHAR
+	db	0x72
+; CHAR
+	db	0x6C
+; CHAR
+	db	0x64
+; CHAR
+	db	0x21
+; CHAR
+	db	0x0A
+; CHAR
+	db	0x00
+; TEXT
+segment	.text
+; ADDR
+	push	dword $_i1
+; CALL
+	call	$_prints
+; TRASH
+	add	esp, -4
 ; LOCAL
 	lea	eax, [ebp+-4]
 	push	eax
